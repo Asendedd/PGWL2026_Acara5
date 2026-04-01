402079658fa1affda56e4ca94856fda595b3859f
Asended <kenobiwahyuhidayat123@gmail.com>
2026-04-02 00:32:59 +0700

    fix duplication in migrations and missing routes that caused CI/CD failures

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PointsController;
 use App\Http\Controllers\PolylinesController;
 use App\Http\Controllers\PolygonsController;
 
-Route::get('/', function () { return view('welcome'); });
+Route::get('/', function () { return view('welcome'); })->name('home');
 Route::get('/map', function () { return view('map'); });
 Route::get('/table', function () {
     return view('table'); // Pastikan filenya resources/views/table.blade.php
@@ -14,3 +14,9 @@ Route::get('/table', function () {
 Route::resource('points', PointsController::class);
 Route::resource('polylines', PolylinesController::class);
 Route::resource('polygons', PolygonsController::class);
+ 
+Route::get('dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+ 
+require __DIR__.'/settings.php';
